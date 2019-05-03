d4274628bfad2215e95b2eb65f560b2a43ffcdb8
JunesuChoi <truekill@naver.com>
2019-05-03 16:13:56 +0900

    add source

---
+++ src/main/java/org/cjs/midterm1/SubscriptionDao.java
@@ -0,0 +1,42 @@
+package org.cjs.midterm1;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SubscriptionDao {
+
+	static final String ADD_SUBSCRIPTION = "INSERT subscription(userId,name,channelId,title) VALUES(?,?,?,?)";
+
+	static final String DELETE_SUBSCRIPTION = "DELETE FROM subscription WHERE (userId, channelId)=(?,?)";
+
+	static final String LIST_SUBSCRIPTIONS = "SELECT channelId,title FROM subscription WHERE userId=?";
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	RowMapper<Subscription> subscriptionRowMapper = new BeanPropertyRowMapper<>(
+			Subscription.class);
+
+	public int addSubscription(Subscription subscription) {
+		return jdbcTemplate.update(ADD_SUBSCRIPTION, subscription.getUserId(),
+				subscription.getName(), subscription.getChannelId(),
+				subscription.getTitle());
+	}
+
+	public int deleteSubscription(Subscription subscription) {
+		return jdbcTemplate.update(DELETE_SUBSCRIPTION,
+				subscription.getUserId(), subscription.getChannelId());
+	}
+
+
+	public List<Subscription> listSubscriptions(String userId) {
+		return jdbcTemplate.query(LIST_SUBSCRIPTIONS, subscriptionRowMapper,
+				userId);
+	}
+}
